6f42583a5da970bb88ca4d5f82114d96c66a97fa
siv2r <siv2ram@gmail.com>
2023-11-14 16:05:29 +0530

    enroll, test: add  API

--- main.py
+++ main.py
@@ -3,39 +3,80 @@
 # Spec details: section 4.1.1 in https://eprint.iacr.org/2017/1155.pdf.
 
 from frost import *
+import secrets
 import unittest
 
 class ExtendedParticipant(FROST.Participant):
     def __init__(self, index, threshold, participants):
         super().__init__(index, threshold, participants)
+        #todo: rename to "nonces"
         self.enrollment_shares = []
+        #todo: rename to "enrolment_shares"
         self.aggregate_enrollment_share = None
+
+    # splits the secret (λ_i.s_i) into t additive shares
+    def generate_enrollment_shares(self, participant_indexes):
+        Q = FROST.secp256k1.Q
+
+        secret = self.lagrange_coefficient(participant_indexes) * self.aggregate_share % Q
+        # generate t random shares that sum to λ_i.s_i
+        self.enrollment_shares = [0 for _ in range(self.participants)]
+
+        for index in participant_indexes:
+            # we assign share for P_i (self) after this loop
+            if index == self.index:
+                continue
+            self.enrollment_shares[index-1] = secrets.randbits(256) % Q
+
+        share_Pi = (secret - sum(self.enrollment_shares)) % Q
+        self.enrollment_shares[self.index - 1] = share_Pi
+
+        assert(sum(self.enrollment_shares) % Q == secret)
+
+    def aggregate_enrollment_shares(self, participant_indexes, enrollment_shares):
+        pass
+
+    def generate_frost_share(self, aggregate_enrollment_shares):
         #TODO: don't forget to update `n` after creating a new enroll share
-
-        # splits λ_i.s_i into n additive shares
-        #todo: this should be `t` right? not `n`. So, where should this param be saved? check frost
-        def generate_enrollment_shares(self):
-
-            # assert the shares sum upto λ_i.s_i
-
-        def aggregate_enrollment_shares(self, enrollment_shares):
-            pass
-
-        def generate_frost_share(self, aggregate_enrollment_shares):
-            pass
+        pass
 
 class EnrollmentTests(unittest.TestCase):
-    def test_generate_frost_share():
-        # generate the frost shares for a new pariticpant
-        # reconstruct the secret using this new share
-        pass
-    def test_sign():
+    def test_generate_frost_share(self):
+        p1 = ExtendedParticipant(index=1, threshold=2, participants=3)
+        p2 = ExtendedParticipant(index=2, threshold=2, participants=3)
+        p3 = ExtendedParticipant(index=3, threshold=2, participants=3)
+
+        # KeyGen
+        p1.init_keygen()
+        p2.init_keygen()
+        p3.init_keygen()
+
+        p1.generate_shares()
+        p2.generate_shares()
+        p3.generate_shares()
+
+        p1.aggregate_shares([p2.shares[p1.index-1], p3.shares[p1.index-1]])
+        p2.aggregate_shares([p1.shares[p2.index-1], p3.shares[p2.index-1]])
+        p3.aggregate_shares([p1.shares[p3.index-1], p2.shares[p3.index-1]])
+
+        p1.derive_public_key([p2.coefficient_commitments[0], p3.coefficient_commitments[0]])
+        p2.derive_public_key([p1.coefficient_commitments[0], p3.coefficient_commitments[0]])
+        pk = p3.derive_public_key([p1.coefficient_commitments[0], p2.coefficient_commitments[0]])
+
+        # Enrollment Protocol
+        participant_indexes = [1, 2]
+        p1.generate_enrollment_shares(participant_indexes)
+        p2.generate_enrollment_shares(participant_indexes)
+
+        # Reconstruct Secret
+
+    def test_sign(self):
         # generate the new frost share
         # check if the sig using this new share is valid
         pass
-    def test_not_in_keygen():
+    def test_not_in_keygen(self):
         ## checks that new pariticpant was not in keygen
         pass
 
 if __name__ == '__main__':
-    pass
+    unittest.main()
