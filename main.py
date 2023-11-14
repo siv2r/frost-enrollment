dc4e69003c5cbb81b2a3c266be8deeae92522ca2
siv2r <siv2ram@gmail.com>
2023-11-14 14:06:32 +0530

    enroll: remove `NewParticipant` class
    I initially though this would allow us to differentiate between the
    old participant and enrolled participant. But we need to impl the sign
    functionalities of old participant in `NewParticipant` again.
    
    So, better to use the same class for both old and enrolled participant
    and simply differntiate them in the comments

--- main.py
+++ main.py
@@ -8,35 +8,33 @@ import unittest
 class ExtendedParticipant(FROST.Participant):
     def __init__(self, index, threshold, participants):
         super().__init__(index, threshold, participants)
-        # splits λ_i.s_i into n additive shares
         self.enrollment_shares = []
         self.aggregate_enrollment_share = None
         #TODO: don't forget to update `n` after creating a new enroll share
 
+        # splits λ_i.s_i into n additive shares
+        #todo: this should be `t` right? not `n`. So, where should this param be saved? check frost
         def generate_enrollment_shares(self):
-            pass
+
+            # assert the shares sum upto λ_i.s_i
 
         def aggregate_enrollment_shares(self, enrollment_shares):
             pass
 
-class NewParticipant:
-    def __init__(self, index, threshold, participants):
-        self.index = index
-        self.threshold = threshold
-        self.participants = participants
-        self.frost_share = None
-
-    def generate_frost_share(self, aggregate_enrollment_shares):
-        pass
+        def generate_frost_share(self, aggregate_enrollment_shares):
+            pass
 
 class EnrollmentTests(unittest.TestCase):
     def test_generate_frost_share():
-        # generate the frost shares for `NewParticpant`
+        # generate the frost shares for a new pariticpant
         # reconstruct the secret using this new share
         pass
     def test_sign():
         # generate the new frost share
         # check if the sig using this new share is valid
+        pass
+    def test_not_in_keygen():
+        ## checks that new pariticpant was not in keygen
         pass
 
 if __name__ == '__main__':
